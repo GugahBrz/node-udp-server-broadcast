8082bc04f41a365a804c3ae4dec103ad1ae0d89d
Gustavo Zanoni <gustavo.zanoni@elevo.io>
2020-12-22 23:48:33 +0100

    Some re-organization

--- src/client.js
+++ src/client.js
@@ -2,13 +2,11 @@ const SERVER_ADDR = 'localhost';
 const SERVER_PORT = 3001;
 
 const dgram = require('dgram');
-const buffer = require('buffer');
 const client = dgram.createSocket({ type: 'udp4', reuseAddr: true });
 
 client.on('error', (err) => client.close());
 
 const message = Buffer.from('Some message.');
-client.send(message, SERVER_PORT, SERVER_ADDR, (err) => {
-    console.log('Message sent.');
-    client.close();
+client.connect(SERVER_PORT, SERVER_ADDR, () => {
+    client.send(message, () => client.close());
 });
--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@ const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
+    console.log(`Server error:\n${err.stack}`);
     server.close();
 });
 
 server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log(`Received: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
     const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
+    console.log(`Server listening on ${address.address}:${address.port}`);
 });
 
 server.bind(PORT);
